e1f35c1de78e8ac9812ada8887fdfb6f29cb0ed5
Qamar5656 <qamar4157@gmail.com>
2025-07-31 23:38:04 +0500

    adding routing details in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import CategoryCard from "./Components/Categories Section/CategoryCard";
 import HeroSlider from "./Components/HomeSlider/HeroSlider";
 import MobileCard from "./Components/Mobile Section/MobileCard";
@@ -7,6 +9,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import NavbarItems from "./Components/Navbar/NavbarItems";
 import TopNavbar from "./Components/Navbar/TopNavbar";
 import Todolist from "./Components/TodoList Section/Todolist";
+import Detailspage from "./Components/Mobile Section/Detailspage";
 
 function App() {
   return (
@@ -16,8 +19,15 @@ function App() {
       <NavbarItems />
       <Todolist />
       <HeroSlider />
-      <MobileItems />
-      {/* <CategoryCard /> */}
+
+      {/* Wrap your routed components inside BrowserRouter */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MobileItems />} />
+          <Route path="/details" element={<Detailspage />} />
+          {/* Add more routes here if needed */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
