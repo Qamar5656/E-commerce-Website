6b3c718865c10f564ad1bb02dd196936e457c022
Qamar5656 <qamar4157@gmail.com>
2025-08-01 10:15:40 +0500

    updating main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
 import HeroSlider from "./Components/HomeSlider/HeroSlider";
-import MobileCard from "./Components/Grocery Items/GroceryCard";
-import MobileItems from "./Components/Grocery Items/GroceryItems";
+import GroceryCard from "./Components/Grocery Items/GroceryCard";
+import GroceryItems from "./Components/Grocery Items/GroceryItems";
 import Navbar from "./Components/Navbar/Navbar";
 import NavbarItems from "./Components/Navbar/NavbarItems";
 import TopNavbar from "./Components/Navbar/TopNavbar";
-import Todolist from "./Components/TodoList Section/Todolist";
+// import Todolist from "./Components/TodoList Section/Todolist";
 
 function App() {
   return (
@@ -13,9 +13,9 @@ function App() {
       <TopNavbar />
       <Navbar />
       <NavbarItems />
-      <Todolist />
+      {/* <Todolist /> */}
       <HeroSlider />
-      <MobileItems />
+      <GroceryItems />
       {/* <CategoryCard /> */}
     </>
   );
