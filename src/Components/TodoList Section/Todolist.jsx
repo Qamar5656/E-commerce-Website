8dd11acfb15c8369ae3c6fdc60ca14a4bd4a48f4
Qamar5656 <qamar4157@gmail.com>
2025-08-01 09:58:22 +0500

    adding todo list

---
+++ src/Components/TodoList Section/Todolist.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+const Todolist = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleTasks = () => {
+    if (todo.trim() === "") {
+      alert("Please enter a task in the input");
+      return;
+    }
+
+    const newTodo = {
+      id: Date.now(), // Unique ID using timestamp
+      text: todo.trim(),
+    };
+
+    setTodos([...todos, newTodo]);
+    setTodo(""); // Reset input
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleTasks();
+    }
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="container py-5">
+      <div className="flex justify-center items-center mb-4">
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+          onKeyDown={handleKeyDown}
+          name="search"
+          className="border p-2"
+        />
+        <button
+          className="border bg-primary text-dark p-2 font-bold cursor-pointer"
+          onClick={handleTasks}
+        >
+          Add Todo
+        </button>
+      </div>
+
+      <ul>
+        {todos.map((item) => (
+          <li
+            key={item.id}
+            className="border my-2 p-2 font-bold flex justify-between"
+          >
+            {item.text}
+            <div className="flex gap-3">
+              <span className="border px-2 cursor-pointer hover:bg-gray-400 hover:text-white rounded-2xl">
+                Update
+              </span>
+              <span
+                className="border px-2 cursor-pointer hover:bg-gray-400 hover:text-white rounded-2xl"
+                onClick={() => deleteTodo(item.id)}
+              >
+                Delete
+              </span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todolist;
