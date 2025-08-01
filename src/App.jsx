c9d0ea36324dd98adead4d29e555f50d893a0bee
Qamar5656 <qamar4157@gmail.com>
2025-08-01 10:20:19 +0500

    adding routing in main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CategoryCard from "./Components/Categories Section/CategoryCard";
 import HeroSlider from "./Components/HomeSlider/HeroSlider";
 import GroceryCard from "./Components/Grocery Items/GroceryCard";
 import GroceryItems from "./Components/Grocery Items/GroceryItems";
 import Navbar from "./Components/Navbar/Navbar";
 import NavbarItems from "./Components/Navbar/NavbarItems";
 import TopNavbar from "./Components/Navbar/TopNavbar";
+import Todolist from "./Components/TodoList Section/Todolist";
+import Detailspage from "./Components/Mobile Section/Detailspage";
 // import Todolist from "./Components/TodoList Section/Todolist";
 
 function App() {
@@ -15,6 +19,15 @@ function App() {
       <NavbarItems />
       {/* <Todolist /> */}
       <HeroSlider />
+
+      {/* Wrap your routed components inside BrowserRouter */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MobileItems />} />
+          <Route path="/details" element={<Detailspage />} />
+          {/* Add more routes here if needed */}
+        </Routes>
+      </BrowserRouter>
       <GroceryItems />
       {/* <CategoryCard /> */}
     </>
