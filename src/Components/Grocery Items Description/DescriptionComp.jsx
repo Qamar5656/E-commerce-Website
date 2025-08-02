204a2ce84798f63901ee8ea89406275da8544f4c
Qamar5656 <qamar4157@gmail.com>
2025-08-02 10:44:26 +0500

    updating description component

---
+++ src/Components/Grocery Items Description/DescriptionComp.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const DescriptionComp = () => {
+  return (
+    <>
+      <div className="py-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto tenetur
+        officiis nemo eaque, perspiciatis ea saepe magni. Quae, mollitia
+        dolores, laboriosam, dignissimos ratione debitis doloremque quaerat
+        soluta sapiente itaque placeat? Lorem ipsum dolor sit amet consectetur,
+        adipisicing elit. Totam omnis adipisci deserunt optio, officia amet
+        autem explicabo quam in mollitia dolorum nobis? Vitae deleniti rerum
+        facilis fugit at deserunt sed.
+      </div>
+    </>
+  );
+};
+
+export default DescriptionComp;
