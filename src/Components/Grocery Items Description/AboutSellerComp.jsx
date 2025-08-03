f44d24f23a1487b203e0f578e1b21d040dfe4803
Qamar5656 <qamar4157@gmail.com>
2025-08-03 23:00:14 +0500

    adding content in about seller component

--- src/Components/Grocery Items Description/AboutSellerComp.jsx
+++ src/Components/Grocery Items Description/AboutSellerComp.jsx
@@ -3,7 +3,24 @@ import React from "react";
 const AboutSellerComp = () => {
   return (
     <>
-      <div>AboutSellerComp</div>
+      <>
+        <div>
+          <h1 className="text-center py-3 font-bold">AboutSellerComp</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+            fugiat maiores provident unde neque perspiciatis quod enim ad.
+            Voluptates, tempora excepturi? Deserunt reprehenderit esse sapiente
+            iste repellendus asperiores laudantium laborum. Lorem ipsum dolor
+            sit amet consectetur adipisicing elit. Nemo sed temporibus quos at
+            dolore explicabo. Tenetur delectus omnis totam ipsam sequi? Esse id,
+            quasi necessitatibus earum quam rerum sit autem? Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit. Dicta perferendis eveniet
+            pariatur magni aperiam omnis, possimus saepe atque recusandae.
+            Itaque in voluptatum, repellat libero voluptate quaerat aliquid
+            explicabo aperiam modi.
+          </p>
+        </div>
+      </>
     </>
   );
 };
