a8cd40c512da3bb9a646d86b1b210dd72503fef6
Qamar5656 <qamar4157@gmail.com>
2025-08-03 22:58:31 +0500

    adding content in shipping component

--- src/Components/Grocery Items Description/ShippingComp.jsx
+++ src/Components/Grocery Items Description/ShippingComp.jsx
@@ -3,7 +3,24 @@ import React from "react";
 const ShippingComp = () => {
   return (
     <>
-      <div>ShippingComp</div>
+      <>
+        <div>
+          <h1 className="text-center font-bold py-3 ">ShippingComp</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+            sequi voluptatum, quis ipsa est obcaecati ratione, inventore
+            assumenda consequuntur sed hic dicta. Fugiat repudiandae beatae
+            repellendus ad minima cum enim? Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Eos maxime inventore culpa distinctio,
+            dolores, quos earum eligendi nisi deserunt adipisci quisquam,
+            pariatur rerum dicta accusamus expedita. Consectetur accusamus
+            cumque possimus. Lorem ipsum dolor sit amet consectetur, adipisicing
+            elit. Aliquam rem odio pariatur non neque at, facere ea porro a
+            officia blanditiis sit eius? Nesciunt temporibus adipisci, eius modi
+            aspernatur voluptatum.
+          </p>
+        </div>
+      </>
     </>
   );
 };
