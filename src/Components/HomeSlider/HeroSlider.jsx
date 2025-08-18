f4c2ac1689c666785fa3daad20730c9e4d3b756a
Qamar5656 <qamar4157@gmail.com>
2025-08-18 18:11:31 +0500

    adding slider using swiper

--- src/Components/HomeSlider/HeroSlider.jsx
+++ src/Components/HomeSlider/HeroSlider.jsx
@@ -1,42 +1,34 @@
-import React, { useState, useEffect } from "react";
-import mobileImg from "../../assets/imgs/mobile.webp";
-import mobileImg3 from "../../assets/imgs/dry_fruit.webp";
-import mobileImg4 from "../../assets/imgs/grocery.webp";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation, Autoplay } from "swiper/modules";
+
+import img1 from "../../assets/imgs/mobile.webp";
+import img2 from "../../assets/imgs/dry_fruit.webp";
+import img3 from "../../assets/imgs/grocery.webp";
 
 const HeroSlider = () => {
-  const images = [mobileImg, mobileImg3, mobileImg4];
-  const [currentIndex, setCurrentIndex] = useState(0);
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 3000);
-
-    return () => clearInterval(interval); // Clean up
-  }, []);
+  const images = [img1, img2, img3];
 
   return (
     <div className="container">
-      <p className="relative text-center">
-        <i
-          className="fa fa-arrow-left text-light bg-transparent hover:bg-blue-500 rounded-full border p-4 absolute left-0 top-60 transform -translate-y-1/2 cursor-pointer"
-          onClick={() =>
-            setCurrentIndex((currentIndex - 1 + images.length) % images.length)
-          }
-        ></i>
-      </p>
-      <img
-        src={images[currentIndex]}
-        alt={`Slide ${currentIndex}`}
-        className="w-full h-auto object-cover rounded-lg"
-      />
-      <p className="relative text-center">
-        <i
-          className="fa fa-arrow-right text-light bg-transparent hover:bg-blue-500 rounded-full border p-4 absolute -top-65 right-0 transform  cursor-pointer"
-          onClick={() => setCurrentIndex((currentIndex + 1) % images.length)}
-        ></i>
-      </p>
+      <Swiper
+        modules={[Navigation, Autoplay]}
+        navigation
+        autoplay={{ delay: 3000, pauseOnMouseEnter: true }}
+        loop
+        className="rounded-lg"
+      >
+        {images.map((img, index) => (
+          <SwiperSlide key={index}>
+            <img
+              src={img}
+              alt={`slide-${index}`}
+              className="w-full h-auto object-cover"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
   );
 };
