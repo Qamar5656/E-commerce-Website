5e69d0adcc090b5dfe20f7eed76b5050e2ccdb6c
Qamar5656 <qamar4157@gmail.com>
2025-08-20 13:06:26 +0500

    updating classes of the topnavbar component

--- src/Components/Navbar/TopNavbar.jsx
+++ src/Components/Navbar/TopNavbar.jsx
@@ -2,26 +2,33 @@ import IconText from "./Icontext";
 
 const TopNavbar = () => {
   return (
-    <div className="w-full bg-primary py-2">
-      <div className="container text-dark flex flex-col gap-3 lg:flex-row lg:gap-0 justify-between">
+    <div className="w-full bg-primary dark:bg-dark text-dark dark:text-primary py-2">
+      <div className="container dark:text-light flex flex-col gap-3 lg:flex-row lg:gap-0 justify-between">
+        {/* Welcome text */}
         <div>
           <p className="sm:text-sm md:text-lg lg:text-lg xl:text-xl">
             Welcome to worldwide Megamart!
           </p>
         </div>
+
+        {/* Right side links */}
         <div className="flex flex-col sm:flex-row gap-2 sm:gap-7 sm:text-sm md:text-lg lg:text-lg xl:text-xl">
           <IconText
-            className="text-secondary fa-location-dot"
-            text="Deliever to 241542"
-          />{" "}
-          <span className="hidden sm:flex items-center opacity-20">|</span>
+            className="text-secondary dark:text-secondary hover:text-tertiary fa-location-dot"
+            text="Deliver to 241542"
+          />
+          <span className="hidden sm:flex items-center text-primary dark:text-tertiary opacity-50">
+            |
+          </span>
           <IconText
-            className="text-secondary fa-truck"
+            className="text-secondary dark:text-secondary hover:text-tertiary dark:hover:text-tertiary fa-truck"
             text="Track your order"
           />
-          <span className="hidden sm:flex items-center opacity-20">|</span>
+          <span className="hidden sm:flex items-center text-primary dark:text-tertiary opacity-50">
+            |
+          </span>
           <IconText
-            className="text-secondary fa-circle-dot"
+            className="text-secondary dark:text-secondary hover:text-tertiary fa-circle-dot"
             text="All offers"
           />
         </div>
